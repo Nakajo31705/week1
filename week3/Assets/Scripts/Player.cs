58c8cfb7ded35005c8653a3944948ba494ce6ae3
nakajo <>
2025-11-07 02:29:12 +0900

    再度マップのテクスチャをいれた。 プレイヤーの移動スクリプト完成。

--- week3/Assets/Scripts/Player.cs
+++ week3/Assets/Scripts/Player.cs
@@ -2,15 +2,42 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Rigidbody2D rb;
+    [SerializeField] private float moveSpeed;   //プレイヤーの移動速度
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        PlayerMove();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// プレイヤーの移動の処理
+    /// </summary>
+    private void PlayerMove()
     {
-        
+        Vector2 pos = transform.position;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            pos += new Vector2(rb.linearVelocity.x, moveSpeed) * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            pos += new Vector2(rb.linearVelocity.x, -moveSpeed) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            pos += new Vector2(moveSpeed, rb.linearVelocity.y) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            pos += new Vector2(-moveSpeed, rb.linearVelocity.y) * Time.deltaTime;
+        }
+
+        transform.position = pos;
     }
 }
--- week3/Assets/Scripts/Stage.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class Stage : MonoBehaviour
-{
-    [SerializeField] private GameObject Wall;
-}
